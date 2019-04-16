6e07b8ba126293739439c8af35c85ba31c483ebb
Jared Noble <jsnoble1@gmail.com>
2019-04-16 14:16:58 -0700

    updated to latest ts-transforms with speed improvments

--- test/match-spec.ts
+++ test/match-spec.ts
@@ -19,32 +19,8 @@ describe('matcher', () => {
     it('can return matching documents', async () => {
         const opConfig = {
             _op: 'watcher',
-            rules: [`${assetName}:transformRules1.txt`],
-            types: { _created: 'date' }
-        };
-
-        const executionConfig = newTestExecutionConfig({
-            assets: [assetName],
-            operations: [opConfig]
-        });
-
-        const data = DataEntity.makeArray([
-            { some: 'data', bytes: 1200 },
-            { some: 'data', bytes: 200 },
-            { some: 'other', bytes: 1200 },
-            { other: 'xabcd' },
-            { _created: '2018-12-16T15:16:09.076Z' }
-        ]);
-
-        const test = await opTest.init({ executionConfig, type });
-        const results =  await test.run(data);
-        expect(results.length).toEqual(4);
-    });
-
-    it('it add metadata to returning docs', async () => {
-        const opConfig = {
-            _op: 'watcher',
-            rules: [`${assetName}:transformRules1.txt`],
+            plugins: ['someAssetId:plugins'],
+            rules: [`${assetName}:matchRules.txt`],
             types: { _created: 'date' }
         };
 
@@ -64,40 +40,7 @@ describe('matcher', () => {
         const test = await opTest.init({ executionConfig, type });
         const results =  await test.run(data);
 
-        expect(results.length).toEqual(4);
-        // @ts-ignore
-        results.forEach((doc: DataEntity) => expect(doc.getMetadata('selectors')).toBeDefined());
+        expect(results.length).toEqual(3);
     });
 
-    it('it can match multiple rules', async () => {
-        const opConfig = {
-            _op: 'watcher',
-            rules: [`${assetName}:transformRules1.txt`],
-            types: { _created: 'date' }
-        };
-
-        const executionConfig = newTestExecutionConfig({
-            assets: [assetName],
-            operations: [opConfig]
-        });
-
-        const data = DataEntity.makeArray([
-            { some: 'data', bytes: 1200, _created: '2018-12-16T15:16:09.076Z' },
-            { some: 'data', bytes: 200 },
-            { some: 'other', bytes: 1200 }
-        ]);
-
-        const rules = {
-            'some:data': true,
-            'some:data AND bytes:>=1000': true,
-            'other:/.*abc.*/ OR _created:>=2018-11-16T15:16:09.076Z': true
-        };
-
-        const test = await opTest.init({ executionConfig, type });
-        const results =  await test.run(data);
-        // each match will be inserted into the results
-        expect(results.length).toEqual(2);
-        // @ts-ignore
-        expect(results[0].getMetadata('selectors')).toEqual(rules);
-    });
 });
